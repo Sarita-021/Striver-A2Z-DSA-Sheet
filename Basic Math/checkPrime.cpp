c7b116a4e502c6971ae63b91395e5d44a4e54f01
Sarita <121181405+Sarita-021@users.noreply.github.com>
2024-06-22 22:29:27 +0530

    Create checkPrime.cpp

---
+++ Basic Math/checkPrime.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public :
+  bool checkPrime(int n){ 
+      int cnt = 0;
+      for(int i = 1; i <= sqrt(n); i++){ 
+          if(n % i == 0){ 
+              cnt = cnt + 1;
+              if(n / i != i){
+                  cnt = cnt + 1;
+              }
+          }
+      }
+  
+      if(cnt == 2){
+          return true;
+      }
+      else{ 
+          return false; 
+      }
+  }
+}
