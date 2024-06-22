c8cd22087b51421e08cac1fdf85b426d5cb0951a
Sarita <121181405+Sarita-021@users.noreply.github.com>
2024-06-22 22:15:30 +0530

    Implemented Count Digits

---
+++ Basic Math/countDigits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int countDigits(int n)
+{
+    int cnt = (int)(log10(n) + 1);
+    return cnt;
+}
+
+int main()
+{
+    int N = 329823;
+    cout << "N: " << N << endl;
+    int digits = countDigits(N);
+    cout << "Number of Digits in N: " << digits << endl;
+    return 0;
+}
