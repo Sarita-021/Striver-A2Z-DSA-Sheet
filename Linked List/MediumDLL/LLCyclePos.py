e0e31e9758aae59f1cb12ac062e87bfe5b27321f
Sarita <121181405+Sarita-021@users.noreply.github.com>
2024-08-15 16:04:56 +0530

    Create LLCyclePos.py

---
+++ Linked List/MediumDLL/LLCyclePos.py
@@ -0,0 +1,26 @@
+'''Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.'''
+
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast and fast.next :
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast :
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+
+        return None
