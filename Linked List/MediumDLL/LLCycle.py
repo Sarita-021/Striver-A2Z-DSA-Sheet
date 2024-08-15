ed113b61d625632bd6afdd5d5f328258b15f6668
Sarita <121181405+Sarita-021@users.noreply.github.com>
2024-08-15 15:13:33 +0530

    Create LLCycle.py

---
+++ Linked List/MediumDLL/LLCycle.py
@@ -0,0 +1,22 @@
+'''Given head, the head of a linked list, determine if the linked list has a cycle in it.'''
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while fast and fast.next :
+            slow = slow.next
+            fast = fast.next.next
+        
+            if slow == fast :
+                return True
+
+        return False
