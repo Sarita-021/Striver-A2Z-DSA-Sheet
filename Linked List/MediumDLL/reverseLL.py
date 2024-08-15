c400c6e0a895908e3be1e5640c6ec1b6aa576a6f
Sarita <121181405+Sarita-021@users.noreply.github.com>
2024-08-15 14:34:01 +0530

    Update reverseLL.py

--- Linked List/MediumDLL/reverseLL.py
+++ Linked List/MediumDLL/reverseLL.py
@@ -9,6 +9,9 @@ class ListNode:
         self.val = val
         self.next = next
 
+
+# Iterative Approach
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         node = None
@@ -20,3 +23,16 @@ class Solution:
             head = temp
         
         return node
+    
+
+# Recursive Approach
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
