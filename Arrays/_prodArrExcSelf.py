2450fa64430e53eb53cf58a42c72bd2e493c4503
Sarita <121181405+Sarita-021@users.noreply.github.com>
2024-11-20 22:30:29 +0530

    Create _prodArrExcSelf.py

---
+++ Arrays/_prodArrExcSelf.py
@@ -0,0 +1,22 @@
+'''Given an integer array nums, return an array answer such that answer[i] is equal to 
+the product of all the elements of nums except nums[i].'''
+
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        output = [1] * len(nums)
+        
+        left = 1
+        for i in range(len(nums)):
+            output[i] *= left
+            left *= nums[i]
+        
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            output[i] *= right
+            right *= nums[i]
+    
+        return output 
+        
