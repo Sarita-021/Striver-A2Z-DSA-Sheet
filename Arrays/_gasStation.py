6a2a109e72de69eb9ad60a26a2356d9aaec5ac05
Sarita <121181405+Sarita-021@users.noreply.github.com>
2024-11-20 23:49:47 +0530

    Create _gasStation.py

---
+++ Arrays/_gasStation.py
@@ -0,0 +1,23 @@
+'''There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its
+next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+'''
+
+from typing import List
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+                
+        curernt_gas = 0
+        start = 0
+        for i in range(len(gas)):
+            curernt_gas += gas[i] - cost[i]
+            if curernt_gas < 0:
+                curernt_gas = 0
+                start = i + 1
+
+        return start
