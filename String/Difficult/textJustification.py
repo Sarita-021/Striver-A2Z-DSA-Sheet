47a53c4901b41ae8da213a1a490c848dadeb79c0
Sarita <121181405+Sarita-021@users.noreply.github.com>
2025-01-10 19:14:01 +0530

    Update textJustification.py

---
+++ String/Difficult/textJustification.py
@@ -0,0 +1,28 @@
+'''Given an array of strings words and a width maxWidth, format the text such that each line has exactly 
+maxWidth characters and is fully (left and right) justified.
+
+Input: words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16
+Output:
+[
+   "This    is    an",
+   "example  of text",
+   "justification.  "
+]
+
+https://leetcode.com/problems/text-justification/?envType=study-plan-v2&envId=top-interview-150'''
+
+from typing import List
+
+
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        res, cur, num_of_letters = [], [], 0
+        for w in words:
+            if num_of_letters + len(w) + len(cur) > maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    cur[i%(len(cur)-1 or 1)] += ' '
+                res.append(''.join(cur))
+                cur, num_of_letters = [], 0
+            cur += [w]
+            num_of_letters += len(w)
+        return res + [' '.join(cur).ljust(maxWidth)]
