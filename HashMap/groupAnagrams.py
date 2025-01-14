7663e32074ab15e48b67d4f9ac564c2268c52b25
Sarita <121181405+Sarita-021@users.noreply.github.com>
2025-01-14 13:43:54 +0530

    Create groupAnagrams.py

---
+++ HashMap/groupAnagrams.py
@@ -0,0 +1,25 @@
+'''Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.'''
+
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
